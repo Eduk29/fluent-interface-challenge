1e880af3937d48d1a077f6c8c2cfc973f81ad325
Eduardo Marques <edu.temarques@gmail.com>
2019-08-06 23:26:19 -0300

    WIP

--- FluentCalculator.js
+++ FluentCalculator.js
@@ -1,7 +1,45 @@
 export default class FluentCalculator {
-  // TODO: implement!
+  constructor(number) {
+    if (!number) {
+      this.number = 0;
+    } else {
+      this.number = number;
+    }
+  }
+
+  equals() {
+    return this.number;
+  }
+
+  plus(value) {
+    if (!this.number) {
+      return this;
+    }
+    return (this.number = this.number + value);
+  }
+
+  minus(value) {
+    if (!this.number) {
+      return this;
+    }
+    this.number = this.number - value;
+  }
+
+  times(value) {
+    if (!this.number) {
+      return this;
+    }
+    this.number = this.number * value;
+  }
+
+  dividedBy(value) {
+    if (!this.number) {
+      return this;
+    }
+    this.number = this.number / value;
+  }
 }
 
-export function createCalculator() {
-  // TODO: implement!
+export function createCalculator(number) {
+  return new FluentCalculator(number);
 }
--- index.js
+++ index.js
@@ -2,5 +2,17 @@ import { createCalculator } from './FluentCalculator.js'
 
 const calc = createCalculator(10)
 
-console.log(calc.plus(12).times(3).minus(1).dividedBy(2).equals())
+calc.plus(1);
+console.log('Sum: ', calc.equals());
+
+// calc.minus(1);
+// console.log('Sub: ', calc.equals());
+
+// calc.times(3);
+// console.log('Mul: ', calc.equals());
+
+// calc.dividedBy(2);
+// console.log('Div: ', calc.equals());
+
+//console.log(calc.plus(12).times(3).minus(1).dividedBy(2).equals())
 console.log(calc)
